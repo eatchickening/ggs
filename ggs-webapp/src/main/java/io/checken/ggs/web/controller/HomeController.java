5baade1679ddfa7c4d82f1e4d66a1a147ac0926b
wungmc <wungcc@gmail.com>
2017-08-22 21:27:48 +0800

    add HomeController.java

---
+++ ggs-webapp/src/main/java/io/checken/ggs/web/controller/HomeController.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C), 2011-2017, 微贷网.
+ */
+package io.checken.ggs.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author wung 2017/8/22.
+ */
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(HttpServletRequest request) {
+        return "index";
+    }
+}
