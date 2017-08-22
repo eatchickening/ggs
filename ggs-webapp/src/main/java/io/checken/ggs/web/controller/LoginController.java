f2317d6f7ba73d9a2335a3365af86ace5b066888
wungmc <wungcc@gmail.com>
2017-08-22 20:28:07 +0800

    增加登录和登出的接口定义

---
+++ ggs-webapp/src/main/java/io/checken/ggs/web/controller/LoginController.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C), 2011-2017, 微贷网.
+ */
+package io.checken.ggs.web.controller;
+
+import io.checken.ggs.common.Result;
+import io.checken.ggs.common.ResultCode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author wung 2017/8/22.
+ */
+@Controller
+@RequestMapping("/ggs/auth")
+public class LoginController {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    @ResponseBody
+    public Result login(String username, String password, HttpServletRequest request, HttpServletResponse response) {
+        logger.info("username=" + username + ", pwd =" + password);
+        if (StringUtils.isEmpty(username) || StringUtils.isEmpty(password)) {
+            return new Result<>(ResultCode.PARAMETER_EMPTY);
+        }
+
+        // 处理逻辑
+
+        return new Result<>(ResultCode.SUCCESS);
+    }
+
+    @RequestMapping(value = "/logout", method = RequestMethod.POST)
+    @ResponseBody
+    public Result logout(HttpServletRequest request, HttpServletResponse response) {
+        // 清除登录状态
+
+        System.out.println("logout...");
+
+        return new Result(ResultCode.SUCCESS);
+    }
+}
