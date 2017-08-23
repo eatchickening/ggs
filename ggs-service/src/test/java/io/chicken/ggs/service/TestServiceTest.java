0558710a03fa88aa3672d0168254d31c376f25ac
mengxiangxiang02 <13162757729@163.com>
2017-08-23 23:00:27 +0800

    提交数据库插入测试

--- ggs-service/src/test/java/io/chicken/ggs/service/TestServiceTest.java
+++ ggs-service/src/test/java/io/chicken/ggs/service/TestServiceTest.java
@@ -1,7 +1,11 @@
 
 package io.chicken.ggs.service;
 
+import io.chicken.ggs.dal.dao.DictionaryMapper;
+import io.chicken.ggs.dal.model.Dictionary;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -11,5 +15,19 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = AppTest.class)
 public class TestServiceTest {
+    @Autowired
+   private DictionaryMapper dictionaryMapper;
+
+    @Test
+    public void testInsert()
+    {
+        Dictionary record =new Dictionary ();
+        record.setId(new Long(2));
+        record.setBizcode("2");
+        record.setBiztype("area");
+        record.setDatavalue("街道");
+        record.setStatus(new Byte("1"));
+        dictionaryMapper.insertSelective(record);
+    }
 
 }
