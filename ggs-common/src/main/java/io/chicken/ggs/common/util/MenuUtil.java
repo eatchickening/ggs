4a5ca5f9f9f77911d0f11c9293bcb8df46ebff75
wungmc <wungcc@gmail.com>
2017-08-24 23:27:06 +0800

    增加权限拦截器

---
+++ ggs-common/src/main/java/io/chicken/ggs/common/util/MenuUtil.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C), 2011-2017.
+ */
+package io.chicken.ggs.common.util;
+
+/**
+ *
+ * @author wung 2017/8/24.
+ */
+public class MenuUtil {
+
+    public static boolean hasAuth(Integer id, String url) {
+        return true;
+    }
+
+    public static String getMenu(Integer id) {
+        return null;
+    }
+}
--- ggs-common/src/main/java/io/chicken/ggs/common/vo/UserInfoVO.java
+++ ggs-common/src/main/java/io/chicken/ggs/common/vo/UserInfoVO.java
@@ -3,8 +3,183 @@
  */
 package io.chicken.ggs.common.vo;
 
+import java.io.Serializable;
+import java.util.Date;
+
 /**
  * @author wung 2017/8/24.
  */
-public class UserInfoVO {
+public class UserInfoVO implements Serializable {
+    private static final long serialVersionUID = -6397638642733642819L;
+
+    private Integer id;
+
+    private String account;
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    private String mobile;
+
+    private String areacode;
+
+    private String departcode;
+
+    private String departName;
+
+    private String postcode;
+
+    private String postName;
+
+    private String organcode;
+
+    private String organName;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getAreacode() {
+        return areacode;
+    }
+
+    public void setAreacode(String areacode) {
+        this.areacode = areacode;
+    }
+
+    public String getDepartcode() {
+        return departcode;
+    }
+
+    public void setDepartcode(String departcode) {
+        this.departcode = departcode;
+    }
+
+    public String getDepartName() {
+        return departName;
+    }
+
+    public void setDepartName(String departName) {
+        this.departName = departName;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getPostName() {
+        return postName;
+    }
+
+    public void setPostName(String postName) {
+        this.postName = postName;
+    }
+
+    public String getOrgancode() {
+        return organcode;
+    }
+
+    public void setOrgancode(String organcode) {
+        this.organcode = organcode;
+    }
+
+    public String getOrganName() {
+        return organName;
+    }
+
+    public void setOrganName(String organName) {
+        this.organName = organName;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    @Override
+    public String toString() {
+        return "UserInfoVO{" +
+                "id=" + id +
+                ", account='" + account + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", mobile='" + mobile + '\'' +
+                ", areacode='" + areacode + '\'' +
+                ", departcode='" + departcode + '\'' +
+                ", departName='" + departName + '\'' +
+                ", postcode='" + postcode + '\'' +
+                ", postName='" + postName + '\'' +
+                ", organcode='" + organcode + '\'' +
+                ", organName='" + organName + '\'' +
+                ", createTime=" + createTime +
+                ", updateTime=" + updateTime +
+                '}';
+    }
 }
