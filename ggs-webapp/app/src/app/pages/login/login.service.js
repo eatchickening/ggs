9c7589a4e2c9d2a7bf550b96b17d1572b195d2f9
songying2009 <songyingjian2009@163.com>
2017-08-24 14:11:33 +0800

    修改POST请求方式，解决跨域访问参数传递问题

--- ggs-webapp/app/src/app/pages/login/login.service.js
+++ ggs-webapp/app/src/app/pages/login/login.service.js
@@ -1,13 +1,17 @@
 (function () {
     'use strict';
 
-    angular.module('chicken.pages.login').factory('LoginService', ['$http', '$q', function($http, $q){
+    angular.module('chicken.pages.login').factory('LoginService', ['$http', '$q', function ($http, $q) {
         return {
             login: function (username, password) {
                 var deferred = $q.defer();
-                $http.post('http://localhost:8080/ggs/auth/login', {
-                    account: username,
-                    password: password
+                $http({
+                    method: 'POST',
+                    url: 'http://localhost:8080/ggs/auth/login',
+                    params: {
+                        account: username,
+                        password: password
+                    }
                 }).then(function (response) {
                     deferred.resolve(response);
                 }).catch(function (response) {
