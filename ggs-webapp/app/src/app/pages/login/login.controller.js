837f495037da9af3d56c92116480c39fbc372641
niuyw119 <niuyw119@163.com>
2017-08-26 09:06:03 +0800

    css udpate for user info

--- ggs-webapp/app/src/app/pages/login/login.controller.js
+++ ggs-webapp/app/src/app/pages/login/login.controller.js
@@ -4,17 +4,17 @@
     angular.module('chicken.pages.login')
         .controller('LoginCtrl', function ($rootScope, $scope, $state, toastr, LoginService) {
             $scope.login = function () {
-                // LoginService.login($scope.user.username, $scope.user.password).then(function(data){
-                //     if (data && data.status === 200) {
-                //         $rootScope.user = {
-                //             username: $scope.user.username,
-                //             password: $scope.user.password
-                //         };
+                LoginService.login($scope.user.username, $scope.user.password).then(function(data){
+                    if (data && data.status === 200 && data.data.code === 0) {
+                        $rootScope.user = {
+                            username: $scope.user.username,
+                            password: $scope.user.password
+                        };
                         $state.go('root.home');
-                //     }
-                // }).catch(function(err) {
-                //     toastr.error(err);
-                // });
+                    }
+                }).catch(function(err) {
+                    toastr.error(err);
+                });
             };
         });
 })();
