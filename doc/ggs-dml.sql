866c7b5a05ddae371bb09c62e6f08d112d9501f2
wungmc <wungcc@gmail.com>
2017-08-27 14:27:27 +0800

    ddl.sql

--- doc/ggs-dml.sql
+++ doc/ggs-dml.sql
@@ -1,10 +1,25 @@
-INSERT INTO `dictionary` (`id`, `biztype`, `bizcode`, `datavalue`, `status`, `remark`) VALUES ('1', 'areacode', '1', '祖庙街道', '0', NULL);
-INSERT INTO `dictionary` (`id`, `biztype`, `bizcode`, `datavalue`, `status`, `remark`) VALUES ('2', 'areacode', '2', '夫子庙', '0', NULL);
-INSERT INTO `dictionary` (`id`, `biztype`, `bizcode`, `datavalue`, `status`, `remark`) VALUES ('3', 'schootype', 'gaozhi', '高职', '0', NULL);
-INSERT INTO `dictionary` (`id`, `biztype`, `bizcode`, `datavalue`, `status`, `remark`) VALUES ('4', 'schootype', 'zhongzhuan', '中专', '0', NULL);
-INSERT INTO `dictionary` (`id`, `biztype`, `bizcode`, `datavalue`, `status`, `remark`) VALUES ('5', 'schootype', 'xiaoxue', '小学', '0', NULL);
+
+insert into `ggs`.`dictionary` ( `bizcode`, `status`, `remark`, `biztype`, `datavalue`) values ( 'QY001', '0', null, 'areacode', '祖庙街道');
+insert into `ggs`.`dictionary` ( `bizcode`, `status`, `remark`, `biztype`, `datavalue`) values ( 'QY002', '0', null, 'areacode', '夫子庙');
+insert into `ggs`.`dictionary` ( `bizcode`, `status`, `remark`, `biztype`, `datavalue`) values ( 'gaozhi', '0', null, 'schootype', '高职');
+insert into `ggs`.`dictionary` ( `bizcode`, `status`, `remark`, `biztype`, `datavalue`) values ( 'zhongzhuan', '0', null, 'schootype', '中专');
+insert into `ggs`.`dictionary` ( `bizcode`, `status`, `remark`, `biztype`, `datavalue`) values ( 'xiaoxue', '0', null, 'schootype', '小学');
 
 
 INSERT INTO `student` (`id`, `studentid`, `name`, `sex`, `award`, `score`, `moral`, `intellectual`, `physical`, `schoolcode`, `remark`) VALUES ('1', '12354562', '孟祥祥', '男', NULL, NULL, NULL, NULL, NULL, 'jialidun', NULL);
 INSERT INTO `school` (`id`, `schoolcode`, `name`, `areacode`, `schooltype`) VALUES ('1', 'jialidun', '家里蹲大学', '2', 'zhongzhuan');
 INSERT INTO  `school` (`id`, `schoolcode`, `name`, `areacode`, `schooltype`) VALUES ('2', 'shannongnongda', '山东农大', '1', 'gaozhi');
+
+
+insert into `ggs`.`organization` ( `organcode`, `areacode`, `name`, `remark`) values ( 'JG001', 'QY001', '机构1', null);
+insert into `ggs`.`organization` ( `organcode`, `areacode`, `name`, `remark`) values ( 'JG002', 'QY002', '机构2', null);
+insert into `ggs`.`organization` ( `organcode`, `areacode`, `name`, `remark`) values ( 'JG003', 'QY001', '机构3', null);
+
+
+insert into `ggs`.`depart` ( `organcode`, `departcode`, `name`, `remark`) values ( 'JG001', 'BM001', '部门1', null);
+insert into `ggs`.`depart` ( `organcode`, `departcode`, `name`, `remark`) values ( 'JG001', 'BM002', '部门2', null);
+
+
+insert into `ggs`.`post` ( `departcode`, `postcode`, `name`, `remark`) values ( 'BM001', 'GW001', '岗位1', null);
+insert into `ggs`.`post` ( `departcode`, `postcode`, `name`, `remark`) values ( 'BM001', 'GW002', '岗位2', null);
+
