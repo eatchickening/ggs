df77b5e2ce7962eaa7bf8fbe6373a18ef6f24e81
TarzanQll <kingnyh@163.com>
2017-08-30 15:59:47 +0800

    "change schoolname to schoolcode"

--- ggs-webapp/src/main/java/io/chicken/ggs/web/controller/TeacherController.java
+++ ggs-webapp/src/main/java/io/chicken/ggs/web/controller/TeacherController.java
@@ -8,6 +8,10 @@ import io.chicken.ggs.common.vo.TeacherQueryParam;
 import io.chicken.ggs.dal.model.Student;
 import io.chicken.ggs.dal.model.Teacher;
 import io.chicken.ggs.service.TeacherService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,14 +24,20 @@ import java.util.Map;
 /**
  * Created by nyh on 8/24/17.
  */
-@Controller
+@RestController
+@Api(description = "查询教师列表")
 @RequestMapping("/ggs/teacher")
 public class TeacherController {
   private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
   @Autowired
   private TeacherBusiness teacherBusiness;
 
-  @ResponseBody
+  @ApiOperation(value="获取教师列表", notes="默认参数为空\"\"")
+  @ApiImplicitParams({
+          @ApiImplicitParam(name = "name", value = "教师姓名", required = true, dataType = "String"),
+          @ApiImplicitParam(name = "pageNum", value = "单页个数", required = true, dataType = "String"),
+          @ApiImplicitParam(name = "pageSize", value = "页数", required = true, dataType = "String"),
+  })
   @RequestMapping(value="/list", method = RequestMethod.POST)
   public Result teacherList(@RequestBody TeacherQueryParam teaParam){
     return teacherBusiness.queryTeacherList(teaParam);
