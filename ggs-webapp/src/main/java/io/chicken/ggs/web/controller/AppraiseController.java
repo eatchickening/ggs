8b083997be5209c28182e362e0cf8894bfa3c40f
mengxiangxiang02 <13162757729@163.com>
2017-09-02 21:51:18 +0800

    修改评优奖项接口

--- ggs-webapp/src/main/java/io/chicken/ggs/web/controller/AppraiseController.java
+++ ggs-webapp/src/main/java/io/chicken/ggs/web/controller/AppraiseController.java
@@ -16,9 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Created by nyh on 8/31/17.
  *
- * 评优活动管理
+ * 评优奖项管理
  */
-@Api(description = "评优活动管理")
+@Api(description = "评优奖项管理")
 @RestController
 @RequestMapping("/ggs/appr")
 public class AppraiseController {
@@ -27,7 +27,7 @@ public class AppraiseController {
     @Autowired
     private AppraiseBusiness appraiseBusiness;
 
-    @ApiOperation(value = "获取评优列表", notes="默认name参数为空, pageSize pageNum不能为空")
+    @ApiOperation(value = "获取评优奖项列表", notes="默认name参数为空, pageSize pageNum不能为空")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "name", value = "评优名称", required = false, dataType = "String", paramType = "form"),
             @ApiImplicitParam(name = "pageNum", value = "每页显示个数", required = true, dataType = "String", paramType = "form"),
