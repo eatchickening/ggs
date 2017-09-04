d55d80d6eede844d579840a0fbc8e6f6fea6bc2a
wungmc <wungcc@gmail.com>
2017-09-04 23:07:10 +0800

    评优表

--- doc/ggs-ddl评优表.sql
+++ doc/ggs-ddl评优表.sql
@@ -73,33 +73,33 @@ CREATE TABLE award_file (
 
 
 --  评优活动表
-DROP  TABLE  IF EXISTS AppraisedActivity;
-CREATE TABLE AppraisedActivity (
-    id bigint NOT NULL AUTO_INCREMENT,
-    activitycode varchar(20)  COMMENT '活动编码',
-    activityname varchar(200)  COMMENT '活动名称',
-    appraisecode varchar(40)  COMMENT '评优编码',
-    isward   varchar(2)  COMMENT '是否有奖项',
-    begindate varchar(20)  COMMENT '活动开始日期',
-    endate varchar(20)  COMMENT '活动结束日期',
-    signEnddate  varchar(20)  COMMENT '活动报名截止日期',
-    remark TEXT COMMENT '备注',
-    activitystatus varchar(20)  COMMENT '活动状态 1：未开始 2进行中 3已结束',
-    creator varchar(40)  COMMENT '创建人',
-    create_time datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    PRIMARY KEY (id)
+DROP  TABLE  IF EXISTS Appraised_Activity;
+CREATE TABLE `Appraised_Activity` (
+    `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+    `activity_name` varchar(200) DEFAULT NULL COMMENT '活动名称',
+    `appraise_id` bigint(20) DEFAULT NULL COMMENT '评优id',
+    `begin_date` timestamp NULL DEFAULT NULL COMMENT '活动开始日期',
+    `end_date` timestamp NULL DEFAULT NULL COMMENT '活动结束日期',
+    `sign_End_date` timestamp NULL DEFAULT NULL COMMENT '活动报名截止日期',
+    `remark` text COMMENT '活动说明',
+    `activity_status` int(11) DEFAULT NULL COMMENT '活动状态 1：未开始(保留) 2进行中 3已结束',
+    `creator` varchar(40) DEFAULT NULL COMMENT '创建人',
+    `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `update_time` timestamp NULL DEFAULT NULL,
+    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='评优活动表';
 
 --  评优活动材料
 DROP  TABLE  IF EXISTS activity_file;
-CREATE TABLE activity_file (
-    id bigint NOT NULL AUTO_INCREMENT,
-    activitycode varchar(40)  COMMENT '所属评优活动编码',
-    filename varchar(200)  COMMENT '材料名称',
-    filetype varchar(200)  COMMENT '材料类型',
-    filepath  varchar(300)  COMMENT '材料路径',
-    filedetail  varchar(300)  COMMENT '材料说明',
-    remark varchar(500) COMMENT '备注',
-    create_time datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='评优活动材料';
+CREATE TABLE `activity_file` (
+    `id` bigint(20) NOT NULL AUTO_INCREMENT,
+    `activity_id` bigint(20) DEFAULT NULL COMMENT '所属评优活动id',
+    `orig_file_name` varchar(200) DEFAULT NULL COMMENT '原材料名称',
+    `sys_file_name` varchar(50) DEFAULT NULL COMMENT '系统材料名称（唯一）',
+    `file_type` varchar(200) DEFAULT NULL COMMENT '材料类型(扩展名)',
+    `file_path` varchar(300) DEFAULT NULL COMMENT '材料路径',
+    `file_detail` varchar(300) DEFAULT NULL COMMENT '材料说明',
+    `remark` varchar(500) DEFAULT NULL COMMENT '备注',
+    `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='评优活动材料表';
